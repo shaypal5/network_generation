9eae3e6af07394b5998a6be50c3faa7b749a9846
shaypal5 <shaypal5@gmail.com>
2015-12-21 12:11:49 +0200

    First commit - empty NetworkInterface, and git files.

---
+++ NetworkGeneration/src/palache/networkgeneration/Network.java
@@ -0,0 +1,16 @@
+package palache.networkgeneration;
+
+/**
+ * An interface that represents a network - a collection of nodes connected with edges.
+ * @author ShayPalachy
+ */
+public interface Network {
+
+   /**
+    * @return the number of nodes in this network.
+    */
+   public int getNetworkSize();
+   
+   
+   
+}
