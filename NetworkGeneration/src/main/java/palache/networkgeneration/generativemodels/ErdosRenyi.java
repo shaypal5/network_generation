30b5655f9b3a61bdc28759bcd66bd1f6ad528dd8
shaypal5 <shaypal5@gmail.com>
2016-01-05 21:31:17 +0200

    Fixed a bug in getGnkNetworkWithConfigurationModel of uneven degrees.

--- NetworkGeneration/src/main/java/palache/networkgeneration/generativemodels/ErdosRenyi.java
+++ NetworkGeneration/src/main/java/palache/networkgeneration/generativemodels/ErdosRenyi.java
@@ -1,8 +1,11 @@
 package palache.networkgeneration.generativemodels;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.function.BiConsumer;
+import java.util.function.IntSupplier;
 
+import palache.commons.arrays.ArraysUtils;
 import palache.networkgeneration.networks.MatrixBasedWeightedNetwork;
 import palache.networkgeneration.networks.NeighborListBasedWeightedNetwork.NeighborListBasedWeightedNetworkBuilder;
 import palache.networkgeneration.networks.Network;
@@ -52,4 +55,61 @@ public class ErdosRenyi {
       }
    }
    
+   /**
+    * @param n The number of nodes in the resulting network.
+    * @param degreeSupplier An object that can supply at least n integers to be used as the degrees of nodes
+    * in the network.
+    * @return a network generated using the G(n,k) configuration model using the given parameters.
+    */
+   public static Network getGnkNetworkWithConfigurationModel(int n, IntSupplier degreeSupplier){
+      int sumDegrees = 0; //# of edges in the network
+      int[] degrees = new int[n];
+      for (int i=0; i<n; i++){
+         int currentDegree = degreeSupplier.getAsInt();
+         sumDegrees += currentDegree;
+         degrees[i] = currentDegree;
+      }
+      if ((sumDegrees % 2) != 0){
+         degrees[n-1]++;
+      }
+      System.out.println("sumDegrees = "+sumDegrees);
+      int m = sumDegrees / 2; //# of edges in the network
+      Network network;
+      if (sumDegrees/(double)n > (Math.log(n)/n) ){ // if network is more likely to be dense
+         double[][] edgeMatrix = new double[n][n];
+         generateGnkNetworkByDegreeSequence(n, m, degrees, (i,j) -> edgeMatrix[i][j] = 1);
+         network = new MatrixBasedWeightedNetwork(n,edgeMatrix);
+      } else {
+         NeighborListBasedWeightedNetworkBuilder netBuilder = 
+               NeighborListBasedWeightedNetworkBuilder.getNewBuilder(n);
+         generateGnkNetworkByDegreeSequence(n, m, degrees, (i,j) -> netBuilder.addWeightedEdge(i, j, 1));
+         network = netBuilder.buildNetwork();
+      }
+      return network;
+   }
+   
+   private static void generateGnkNetworkByDegreeSequence(int n, int m, int[] degrees, BiConsumer<Integer,Integer> addEdgeFunction){
+      System.out.println("n="+n+", m="+m+", degrees="+Arrays.toString(degrees));
+//      throw new RuntimeException("n="+n+", m="+m+", degrees="+degrees);
+      int[] stubs = new int[2*m];
+      int stubCounter = 0;
+      for (int i=0; i<n; i++){
+         System.out.println("i="+i+", n="+n+", stubCounter="+stubCounter);
+         int degCounter = 0;
+         while (degCounter < degrees[i]){
+            try {               
+               stubs[stubCounter] = i;
+            } catch (ArrayIndexOutOfBoundsException e){
+               System.out.println("Exception with n="+n+", m="+m+", i="+i+", stubCounter="+stubCounter);
+               return;
+            }
+            stubCounter++;
+            degCounter++;
+         }
+      }
+      ArraysUtils.shuffleIntArray(stubs);
+      for (int j=0; j<2*m; j+=2){
+         addEdgeFunction.accept(stubs[j], stubs[j+1]);
+      }
+   }
 }
--- NetworkGeneration/src/test/java/palache/networkgeneration/generativemodels/ErdosRenyiTest.java
+++ NetworkGeneration/src/test/java/palache/networkgeneration/generativemodels/ErdosRenyiTest.java
@@ -3,12 +3,15 @@ package palache.networkgeneration.generativemodels;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
+import java.util.function.IntSupplier;
 
+import org.apache.commons.math3.distribution.ParetoDistribution;
 import org.junit.Test;
 
 import palache.networkgeneration.networks.MatrixBasedWeightedNetwork;
 import palache.networkgeneration.networks.NeighborListBasedWeightedNetwork;
 import palache.networkgeneration.networks.Network;
+import palache.networkgeneration.networks.WeightedNetwork;
 
 /**
  * @author ShayPalachy
@@ -19,6 +22,8 @@ public class ErdosRenyiTest {
    @SuppressWarnings("unused")
    //Only used to init static  members of the ErdosRenyi class.
    private static Network initNet = ErdosRenyi.getGnpNetwork(2, 0);
+   
+   private static int NUM_OF_BUILDS_FOR_BUILD_TIME = 1000;
 
    /**
     * Test method for {@link palache.networkgeneration.generativemodels.ErdosRenyi#getGnpNetwork(int, double)}.
@@ -27,7 +32,9 @@ public class ErdosRenyiTest {
    @Test
    public void testGnpDenseNetworkBuildTime() {
       int n = 100;
-      ErdosRenyi.getGnpNetwork(n, 0.9);
+      for (int i=0; i<NUM_OF_BUILDS_FOR_BUILD_TIME; i++){         
+         ErdosRenyi.getGnpNetwork(n, 0.9);
+      }
    }
 
    /**
@@ -36,13 +43,14 @@ public class ErdosRenyiTest {
    @SuppressWarnings("static-method")
    @Test
    public void testGetGnpNetworkWithDenseNetwork() {
+      System.out.println("===================testGetGnpNetworkWithDenseNetwork()=================");
       int n = 100;
       Network net = ErdosRenyi.getGnpNetwork(n, 0.9);
       assertTrue(net instanceof MatrixBasedWeightedNetwork);
-      MatrixBasedWeightedNetwork weightedNet = (MatrixBasedWeightedNetwork) net;
-      for (int i=0; i<n; i++){
-         System.out.println(Arrays.toString(weightedNet.getDenseEdgeWeights(i)));
-      }
+//      MatrixBasedWeightedNetwork weightedNet = (MatrixBasedWeightedNetwork) net;
+//      for (int i=0; i<n; i++){
+//         System.out.println(Arrays.toString(weightedNet.getDenseEdgeWeights(i)));
+//      }
       System.out.println("=====================================================");
    }
 
@@ -54,7 +62,9 @@ public class ErdosRenyiTest {
    public void testGnpSparseNetworkBuildTime() {
       int n = 100;
       double p = 0.9*(1.0/n);
-      ErdosRenyi.getGnpNetwork(n, p);
+      for (int i=0; i<NUM_OF_BUILDS_FOR_BUILD_TIME; i++){          
+         ErdosRenyi.getGnpNetwork(n, p);
+      }
    }
 
    /**
@@ -63,14 +73,49 @@ public class ErdosRenyiTest {
    @SuppressWarnings("static-method")
    @Test
    public void testGetGnpNetworkWithSparseNetwork() {
+      System.out.println("==============testGetGnpNetworkWithSparseNetwork()======================");
       int n = 100;
       double p = 0.9*(1.0/n);
       Network net = ErdosRenyi.getGnpNetwork(n, p);
       assertTrue(net instanceof NeighborListBasedWeightedNetwork);
-      NeighborListBasedWeightedNetwork weightedNet = (NeighborListBasedWeightedNetwork) net;
+//      NeighborListBasedWeightedNetwork weightedNet = (NeighborListBasedWeightedNetwork) net;
+//      for (int i=0; i<n; i++){
+//         System.out.println(Arrays.toString(weightedNet.getDenseEdgeWeights(i)));
+//      }
+      System.out.println("=====================================================");
+   }
+
+   /**
+    * Test method for {@link palache.networkgeneration.generativemodels.ErdosRenyi#getGnpNetwork(int, double)}.
+    */
+   @SuppressWarnings("static-method")
+   @Test
+   public void testGetGnkNetworkWithConfigurationModel() {
+      System.out.println("===========================testGetGnkNetworkWithConfigurationModel==========================");
+      int n = 100;
+      IntSupplier degreeSupplier = new IntSupplier(){         
+         private ParetoDistribution dist = new ParetoDistribution(1,2.4);
+         @Override
+         public int getAsInt() {
+            return Math.max(((int)Math.floor(dist.sample()))-1,0);
+         }         
+      };
+//      ParetoDistribution dist = new ParetoDistribution(1,2.9);
+//      for (int d=0; d<1000; d++){
+//         System.out.print(degreeSupplier.getAsInt()+",");
+//      }
+//      System.out.println();
+//      for (int d=0; d<1000; d++){
+//         System.out.print(dist.sample()+",");
+//      }
+//      System.out.println();
+      Network net = ErdosRenyi.getGnkNetworkWithConfigurationModel(n, degreeSupplier);
+      assertTrue(net instanceof WeightedNetwork);
+      WeightedNetwork weightedNet = (WeightedNetwork) net;
       for (int i=0; i<n; i++){
          System.out.println(Arrays.toString(weightedNet.getDenseEdgeWeights(i)));
       }
+      System.out.println("=====================================================");
    }
 
 }
